52133e5478481400586964737962e8bcf88df133
Ningchen_Ubuntu <ningchen.ma@mail.mcgill.ca>
2022-07-28 00:51:04 -0400

    added two publishers

--- mqtt_publish2.py
+++ mqtt_publish2.py
@@ -4,11 +4,11 @@ from random import randrange, uniform
 import time
 
 mqttBroker = "mqtt.eclipseprojects.io"
-client = mqtt.Client("Temperature_Inside")
+client = mqtt.Client("Temperature_Outside")
 client.connect(mqttBroker)
 
 while True:
-    randomNumber = uniform(20.0,25.0)
+    randomNumber = uniform(0.0,10.0)
     client.publish("Temperature",randomNumber)
     print("Just published",str(randomNumber),"to topic Temperatrue")
     time.sleep(2)
